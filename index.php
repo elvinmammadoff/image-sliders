65877dd0e4c3f237e7aac8bce879f0535c2e976b
Elvin Mammadov <elvinmammadoff@gmail.com>
2023-07-30 17:54:32 +0400

    image sliders

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>BootSlider - Bootstrap Image Slider</title>
+    <title>BootSlider - Bootstrap Image Sliders</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
     <link href="assets/css/style.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
 </head>
 <body>
-<a href="">Business Image Slider</a>
+<a href="/business-slider">Business Hero Slider</a>
 </body>
 </html>
